3b40c1653fb6a4bc43ebff59ef6a7164ed8cd60c
Jeffery Amartey <amarteyjeffery@gmail.com>
2023-07-13 08:32:24 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,10 @@
 /**
  * main - Print program using puts function
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
