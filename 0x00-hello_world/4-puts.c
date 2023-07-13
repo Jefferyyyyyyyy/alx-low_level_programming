8ce6cbaa4257ea40df3bcc4ccd08d3bc0ddc1601
Jeffery Amartey <amarteyjeffery@gmail.com>
2023-07-13 05:17:15 -0700

    Hello, put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - Print program using puts function
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
