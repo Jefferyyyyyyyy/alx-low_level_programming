96ca62a9dd9889b45e26d16290494e6a656af3e9
Jeffery Amartey <amarteyjeffery@gmail.com>
2023-07-17 04:48:40 -0700

    The success combination in business is: Do what you do better... and: do more of what you do

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,34 +1,46 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Return: 0 (Success)
- * Description: Program that prints all possible different combinations of three digits
-*/
+ * main - Entry point of the program
+ *
+ * Description: Prints all possible different combinations of three digits
+ *              in ascending order, separated by commas, and a space
+ *              using putchar.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int i, j, k;
+    int i, j, k;
 
-	for (i = 0; i < 10; i++)
-	{
-		for (j = i + 1; j < 10; j++)
-		{
-			for (k = j + 1; k < 10; k++)
-			{
-				if (i != j && i != k && j != k)
-				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(k + '0');
-				}
+    /* Loop for the first digit (i) */
+    for (i = 0; i <= 7; i++)
+    {
+        /* Loop for the second digit (j) */
+        for (j = i + 1; j <= 8; j++)
+        {
+            /* Loop for the third digit (k) */
+            for (k = j + 1; k <= 9; k++)
+            {
+                /* Print the three digits using putchar */
+                putchar(i + '0');
+                putchar(j + '0');
+                putchar(k + '0');
 
-				if (i != 7 || j != 8 || k != 9)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+                /* Check if it's the last combination */
+                if (!(i == 7 && j == 8 && k == 9))
+                {
+                    /* Print comma and space */
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+
+    /* Print a new line */
+    putchar('\n');
+
+    return 0;
 }
+
