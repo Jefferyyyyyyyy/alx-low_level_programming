6c3c4a2d7dfc6e1e6730f448d04ee202daeea2df
Jeffery Amartey <amarteyjeffery@gmail.com>
2023-07-24 08:29:20 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - Uses pointer to updates the value it points to to 98
+ * @n: A pointer to the integer to be updated
+ * Return: Always 0.
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
