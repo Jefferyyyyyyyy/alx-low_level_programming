34fa44a243626f0e751dae01b6bc0dda6ee3064b
Jeffery Amartey <amarteyjeffery@gmail.com>
2023-07-25 04:58:56 -0700

    strcpy

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,7 +6,6 @@
 
 int _putchar(char c);
 void reset_to_98(int *n);
-void print_number(int n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
@@ -15,7 +14,6 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-int _atoi(char *s);
-char *_strcpy(char *dest, const char *src);
+char *_strcpy(char *dest, char *src);
 
 #endif
